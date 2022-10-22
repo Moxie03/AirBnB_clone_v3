a31d7a4122b5cbc9e164fc0dfa7a569744476225
Joshua Oloton <olotonjoshua@gmail.com>
2022-10-23 00:09:36 +0100

    added json 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
@@ -13,6 +13,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_appcontext(code):
     """teardown_appcontext"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
