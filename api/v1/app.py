8b157113ce199ce11f5d3dfd2983d6d21847697d
Joshua Oloton <olotonjoshua@gmail.com>
2022-10-26 15:27:14 +0100

    corrected errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-
-from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from models import storage
 import os
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
-app.url_map.strict_slashes = False
-
-# flask server environmental setup
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
-
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
@@ -25,13 +19,9 @@ def teardown_appcontext(code):
 
 
 @app.errorhandler(404)
-def not_found(e):
-    """404 error handler"""
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App
-    """
-    app.run(host=host, port=port)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
