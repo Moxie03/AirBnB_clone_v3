40a90db81848132dd48335d524295d027e9d3c41
emekeo <gistwithcollins@gmail.com>
2023-02-15 11:06:57 -0800

    airbnb

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,7 +20,7 @@ def teardown_appcontext(code):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return make_response(jsonify({'error': 'Not '}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
