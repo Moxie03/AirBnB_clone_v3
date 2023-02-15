6f3e797f6795996870784dddff77388adb189bb0
emekeo <gistwithcollins@gmail.com>
2023-02-15 11:05:14 -0800

    airbnb

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" holds class User"""
+""" holds class Users"""
 import hashlib
 import models
 from models.base_model import BaseModel, Base
@@ -10,7 +10,7 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """Representation of a user"""
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -26,7 +26,7 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """initializes users"""
         super().__init__(*args, **kwargs)
 
     @property
